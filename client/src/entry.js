3349990533bbe594bbc5bbc71e5ecb1e842455ae
Ian Henderson <ianwhen@gmail.com>
2015-11-25 12:57:39 -0800

    Add basic query functionality

---
+++ client/src/entry.js
@@ -0,0 +1,69 @@
+(function(window){
+  // Element refs
+  var voteSection = document.getElementById('vote');
+  var voteResults = document.getElementById('vote-results');
+  var queryBox = document.getElementById('queryBox');
+  var queryInput = document.getElementById('queryInput');
+  var results = document.getElementById('results');
+
+  // Helpers
+  function handleVote(e){
+    var voteValue = +e.target.dataset.value;
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    var n = +queryInput.value;
+    getWords(n)
+      .then(function(data){
+        clearTable();
+        buildTable(data);
+      });
+  }
+
+  // Attaching listeners
+  function listenForVote(){
+    voteSection.addEventListener('click', handleVote);
+  }
+
+  function listenForSubmit(){
+    queryBox.addEventListener('submit', handleSubmit);
+  }
+
+  // Build results
+  function buildTable(data) {
+    var ol = document.createElement('ol');
+    attachItems(ol, data);
+    results.appendChild(ol);
+  }
+
+  function attachItems(el, data) {
+    return data.reduce(function(el, datum){
+      var li = document.createElement('li');
+      li.innerText = datum;
+      el.appendChild(li);
+      return el;
+    }, el);
+  }
+
+  function clearTable() {
+    while (results.firstChild) {
+      results.removeChild(results.firstChild);
+    }
+  }
+
+  // AJAX stuff
+  function getWords(n){
+    return reqwest('/api/words/' + n)
+  }
+
+  // Initialize app
+  function init(){
+    listenForVote();
+    listenForSubmit();
+    // window.getWords = getWords;
+  }
+
+  // start app on DOM ready
+  document.addEventListener('DOMContentLoaded', init);
+})(window)
